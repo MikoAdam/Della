6967156d5cd1d30ddc8032eba31d441019f45f9a
adam <adammiko98@gmail.com>
2022-01-24 13:15:49 +0100

    UI core design.

--- app/src/main/java/com/della/MainActivity.kt
+++ app/src/main/java/com/della/MainActivity.kt
@@ -3,36 +3,168 @@ package com.della
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.della.ui.theme.DellaTheme
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.della.screens.*
 
 class MainActivity : ComponentActivity() {
+    @ExperimentalMaterialApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DellaTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+
+            val navController = rememberNavController()
+
+            Scaffold(
+
+                floatingActionButton = {
+                    FloatingActionButton(
+                        onClick = {}
+                    ) {
+                        Icon(Icons.Filled.Create, "")
+                    }
+                },
+
+
+                bottomBar = {
+                    BottomNavigationBar(
+                        items = listOf(
+
+                            BottomNavigationItem(
+                                name = "Savings",
+                                route = "savings",
+                                icon = Icons.Default.Add
+                            ),
+                            BottomNavigationItem(
+                                name = "Incomes",
+                                route = "incomes",
+                                icon = Icons.Default.Add
+                            ),
+                            BottomNavigationItem(
+                                name = "Home",
+                                route = "home",
+                                icon = Icons.Default.Home
+                            ),
+                            BottomNavigationItem(
+                                name = "Expenses",
+                                route = "expenses",
+                                icon = Icons.Default.ShoppingCart
+                            ),
+                            BottomNavigationItem(
+                                name = "Debts",
+                                route = "debts",
+                                icon = Icons.Default.Lock
+                            ),
+                        ),
+                        navController = navController,
+                        onItemClick = {
+                            navController.navigate(it.route)
+                        }
+                    )
+                },
+
+
+                )
+
+            {
+                Navigation(navController = navController)
+
+                LazyColumn(
+                    modifier = Modifier.fillMaxWidth(),
+                    contentPadding = PaddingValues(16.dp)
+                ) {
+                    items(50) {
+
+                        Text("idk $it")
+
+                    }
                 }
+
             }
+
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DellaTheme {
-        Greeting("Android")
+fun Navigation(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = "home") {
+        composable("home") {
+            HomeScreen()
+        }
+        composable("incomes") {
+            Incomes()
+        }
+        composable("expenses") {
+            Expenses()
+        }
+        composable("debts") {
+            Debts()
+        }
+        composable("savings") {
+            Savings()
+        }
     }
 }
+
+@ExperimentalMaterialApi
+@Composable
+fun BottomNavigationBar(
+    items: List<BottomNavigationItem>,
+    navController: NavController,
+    modifier: Modifier = Modifier,
+    onItemClick: (BottomNavigationItem) -> Unit
+) {
+    val backStackEntry = navController.currentBackStackEntryAsState()
+    BottomNavigation(
+        modifier = modifier,
+        backgroundColor = Color.LightGray,
+        elevation = 5.dp
+    ) {
+        items.forEach { item ->
+            val selected = item.route == backStackEntry.value?.destination?.route
+            BottomNavigationItem(
+                selected = selected,
+                onClick = { onItemClick(item) },
+                selectedContentColor = Color.Black,
+                unselectedContentColor = Color.Gray,
+                icon = {
+                    Column(horizontalAlignment = CenterHorizontally) {
+
+                        Icon(
+                            imageVector = item.icon,
+                            contentDescription = item.name
+                        )
+
+                        if (selected) {
+                            Text(
+                                text = item.name,
+                                textAlign = TextAlign.Center,
+                                fontSize = 10.sp
+                            )
+                        }
+                    }
+                }
+            )
+        }
+    }
+}
+
